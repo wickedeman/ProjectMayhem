ebc37e4d748fc29576b5141668c0057cc88d73ed
wickedeman <marencocg@gmail.com>
2016-07-20 23:47:44 -0700

    test
    testing uplaods

---
+++ Assets/Project Mayhem/Scripts/PC.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Controller2D))]
+public class PC : MonoBehaviour {
+
+	Controller2D controller;
+
+	void Start(){
+
+		controller = GetComponent<Controller2D> ();
+	}
+}
