6e11b4bacea7c85c95c3094e030699490400ff2c
wickedeman <marencocg@gmail.com>
2017-01-09 01:21:17 -0800

    Adding Example13 to Depot
    added asset to depot

--- Assets/Project Mayhem/Scripts/Examples/Example13.cs
+++ Assets/Project Mayhem/Scripts/Examples/Example13.cs
@@ -9,14 +9,30 @@ public class Example13 : MonoBehaviour {
 	void Start () {
 		Debug.Log(myReplace ("Let us go to the store", "store", "mall"));
 		Debug.Log(myReplace ("He is Sleeping on the couch", "Sleeping", "sitting"));
-
-		
+		Debug.Log(myReplace ("This has a spellngi error", "spellngi", "spelling"));
+		Debug.Log(myReplace ("His name is Tom", "Tom", "john"));
+		Debug.Log(myReplace ("Let us get back to more Coding", "Coding", "algorithms"));
 	}
 
 	public string myReplace(string str, string before, string after)
 	{
-		//after.IsLower (after);
-		after = after.ToUpper ();
+		if ( char.IsLower(before[0]) && char.IsLower(after[0]) )
+			{
+			//Debug.Log ("Lowercase and Lowercase");
+				if (before.Length == 5) 
+				{
+					if (before.Substring (5) == "ngi") 
+					{
+						after = after.Replace (before.Substring (5), "ing");
+					}
+				}				
+			}
+		if (char.IsUpper(before[0]) && char.IsLower(after[0])) 
+			{
+			//Debug.Log ("Uppercase and Lowercase");
+			after = char.ToUpper(after[0]) + after.Substring(1);
+			}
+
 		str = str.Replace (before, after);
 		return str;
 	}
