a36286df857eb59b7307ef953bbb5affb3b3f89b
wickedeman <marencocg@gmail.com>
2017-01-30 01:45:52 -0800

    Welcome to Depot
    Implemented Dash move to project

---
+++ Assets/Project Mayhem/Scripts/Examples/Example15.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example15 : MonoBehaviour {
+
+	// Example using the "?" operator
+	public int direction = 25;
+
+	// Use this for initialization
+	void Start () {
+		test ();
+	}
+
+	public void test()
+	{
+		
+	direction = direction < 1 ? 1 : 0;
+	Debug.Log (" results: " + direction);
+		//Logs Returns: 0 Direction is greater than 1 return 0 
+		// ? operator returns the X value if true otherwise value Y ( ? X : Y ) 
+	}
+}
---
+++ Assets/Project Mayhem/Scripts/Examples/Example16.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example16 : MonoBehaviour {
+	public MoveSetData myMoveSetData;
+	//UFE Test code
+	// Use this for initialization
+	void Start () {
+		OnGameBegin (UFE.GetPlayer1 (), UFE.GetPlayer2 ());
+		myMoveSetData = UFE.GetCurrentMoveSet (UFE.GetPlayer1 ());
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (myMoveSetData.basicMoves.dash.name);
+	}
+
+	public void OnGameBegin(CharacterInfo player1, CharacterInfo player2)
+	{
+		Debug.Log(player1.characterName + " - " + player1.playerNum);
+		Debug.Log(player2.characterName + " - " + player2.playerNum);
+	}
+}
